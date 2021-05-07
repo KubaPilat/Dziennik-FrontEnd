26aea6a9fa759eec73235c5f49a5f2708b78542c
j.pilat <kubapilat80@gmial.com>
2021-05-07 12:36:22 +0200

    change name sudents to students

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {SudentsComponent} from './sudents/sudents.component';
+import {StudentsComponent} from './students/students.component';
 import {MainComponent} from './main/main.component';
 import {LoginComponent} from './login/login.component';
 import {PlanComponent} from './plan/plan.component';
 
 const routes: Routes = [
   {path: 'main', component: MainComponent},
-  {path: 'sudents', component: SudentsComponent},
+  {path: 'students', component: StudentsComponent},
   {path: 'login', component: LoginComponent},
   {path: 'plan', component: PlanComponent}
 ];
@@ -17,4 +17,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const RoutingComponent = [SudentsComponent, MainComponent];
+export const RoutingComponent = [StudentsComponent, MainComponent];
