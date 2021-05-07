8d7eb8ea1940d257ff3d940f61874067b4c0e591
j.pilat <kubapilat80@gmial.com>
2021-05-07 12:30:37 +0200

    generate main, sudents, login and plan component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {SudentsComponent} from './sudents/sudents.component';
 import {MainComponent} from './main/main.component';
 import {LoginComponent} from './login/login.component';
+import {PlanComponent} from './plan/plan.component';
 
 const routes: Routes = [
   {path: 'main', component: MainComponent},
   {path: 'sudents', component: SudentsComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'plan', component: PlanComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { LoginComponent } from './login/login.component';
+import { PlanComponent } from './plan/plan.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RoutingComponent,
-    LoginComponent
+    LoginComponent,
+    PlanComponent
   ],
   imports: [
     BrowserModule,
