3c6ae256318e552aa95cc31dbe4bb2e1c58fe1b7
j.pilat <kubapilat80@gmial.com>
2021-05-10 12:45:38 +0200

    change HTML table to angular material mat-table add filter with mat-table in class.component and grades.component

--- src/app/plan/plan.component.ts
+++ src/app/plan/plan.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-plan',
   templateUrl: './plan.component.html',
-  styleUrls: ['./plan.component.css']
+  styleUrls: ['./plan.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class PlanComponent implements OnInit {
 
