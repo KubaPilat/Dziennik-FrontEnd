ec5e9f7726b4dfce04a6307b147095c41f5666ee
j.pilat <kubapilat80@gmial.com>
2021-05-10 13:01:58 +0200

    change HTML table to angular material mat-table add filter with mat-table in class.component and grades.component

--- src/app/plan/plan.component.ts
+++ src/app/plan/plan.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
 import {MatTableDataSource} from '@angular/material/table';
-
 @Component({
   selector: 'app-plan',
   templateUrl: './plan.component.html',
@@ -14,11 +13,29 @@ import {MatTableDataSource} from '@angular/material/table';
     ]),
   ],
 })
-export class PlanComponent implements OnInit {
 
-  constructor() { }
+export class PlanComponent {
+  displayedColumns: string[] = ['position'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  ngOnInit(): void {
+  ClassFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
 }
+
+export interface PeriodicElement {
+  position: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: '1A'},
+  {position: '1B'},
+  {position: '1C'},
+  {position: '2A'},
+  {position: '2B'},
+  {position: '2C'},
+  {position: '3A'},
+  {position: '3B'},
+  {position: '3C'},
+];
