b2c1ec682e664200329f3d788a6a76ec6c0fda0a
j.pilat <kubapilat80@gmial.com>
2021-05-11 08:47:13 +0200

    add ma-table with input element to message

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
-export class MessageComponent implements OnInit {
 
-  constructor() {
+export class MessageComponent {
+  displayedColumns: string[] = ['nadawca', 'temat', 'data'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
-  ngOnInit(): void {
-  }
-
 }
+
+export interface PeriodicElement {
+  nadawca: string;
+  temat: string;
+  data: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {nadawca: 'Adam Kruża', temat: 'Zawody', data: '15.02.2021'},
+  {nadawca: 'Adam Kruża', temat: 'a', data: '15.02.2021'},
+  {nadawca: 'Adam Kruża', temat: 's', data: '17.02.2021'},
+  {nadawca: 'Adam Kruża', temat: 'd', data: '18.02.2021'},
+  {nadawca: 'Adam Kruża', temat: 'f', data: '19.02.2021'},
+  {nadawca: 'Adam Kruża', temat: 'g', data: '20.02.2021'},
+  {nadawca: 'Adam Kruża', temat: 'h', data: '21.02.2021'},
+  {nadawca: 'Adam Kruża', temat: 'j', data: '22.02.2021'},
+  {nadawca: 'Adam Kruża', temat: 'k', data: '23.02.2021'},
+  {nadawca: 'Adam Kruża', temat: 'l', data: '24.02.2021'},
+];
