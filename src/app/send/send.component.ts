26e65887b15fbf2813861febb08520e9339154e3
j.pilat <kubapilat80@gmial.com>
2021-05-21 11:55:47 +0200

    finish

--- src/app/send/send.component.ts
+++ src/app/send/send.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Task {
+  name: string;
+  completed: boolean;
+  subtasks?: Task[];
+}
+
 @Component({
   selector: 'app-send',
   templateUrl: './send.component.html',
@@ -14,5 +20,36 @@ export class SendComponent {
     //nie wiedziałem co zrobić więc zrobiłem console.log z danymi z formularza bo czekam na backend
     const data =  {topic: this.topic, text: this.text}
     console.log(data)
+  }
+
+  task: Task = {
+    name: 'Wszyscy',
+    completed: false,
+    subtasks: [
+      {name: 'Kuba', completed: false},
+      {name: 'Mati', completed: false},
+      {name: 'XYZ', completed: false}
+    ]
+  };
+
+  allComplete: boolean = false;
+
+  updateAllComplete() {
+    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
+  }
+
+  someComplete(): boolean {
+    if (this.task.subtasks == null) {
+      return false;
+    }
+    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
+  }
+
+  setAll(completed: boolean) {
+    this.allComplete = completed;
+    if (this.task.subtasks == null) {
+      return;
+    }
+    this.task.subtasks.forEach(t => t.completed = completed);
   }
 }
