1431a94c7710ec4f79b2765a87654c9335246135
Kuba <kubapilat80@gmail.com>
2021-05-26 23:07:33 +0200

    teachers do poprawy

--- src/app/teachers/teachers.component.ts
+++ src/app/teachers/teachers.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
-import {MatTableDataSource} from '@angular/material/table';
+import {ApiService} from '../api.service';
 
 @Component({
   selector: 'app-teachers',
@@ -16,33 +16,34 @@ import {MatTableDataSource} from '@angular/material/table';
 })
 
 export class TeachersComponent {
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
-  columnsToDisplay = ['name', 'Surname'];
-  expandedElement: PeriodicElement | null | undefined;
-  displayedColumns: string[] = ['name',  'Surname'];
-  ELEMENT_DATA: any;
 
-  applyFilter = (event: Event) => {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  columnsToDisplay: string[] = ['name', 'phone', 'email', 'website'];
+  expandedElement: Data | null | undefined;
+  displayedColumns: string[] = ['name',  'email'];
+
+  dataSource: any[] = [];
+
+  constructor(private service: ApiService){
+    this.service.getDataUsers().then(data => {
+      this.dataSource = data;
+    });
   }
+
+
+  // dataSource = new MatTableDataSource(ELEMENT_DATA);
+  // columnsToDisplay = ['name', 'Surname'];
+  // ELEMENT_DATA: any;
+  //
+  // applyFilter = (event: Event) => {
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+  // }
 }
 
-
-export interface PeriodicElement {
+export interface Data {
   name: string;
-  Surname: string;
-  description: string;
+  phone: string;
+  email: string;
+  website: string;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    name: 'Jakub',
-    Surname: 'Piłat',
-    description: `nr. tel:123456789`
-  }, {
-    name: 'Mateusz',
-    Surname: 'Ładyko',
-    description: `nr. tel:987654321`
-  },
-];
