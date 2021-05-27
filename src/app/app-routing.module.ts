9cc64e1740d1272322a08d0f0631d5aaded306a7
j.pilat <kubapilat80@gmial.com>
2021-05-27 12:06:35 +0200

    rest api in message + css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ import {ClassComponent} from './class/class.component';
 import {GradesComponent} from './grades/grades.component';
 import {TeachersComponent} from './teachers/teachers.component';
 import {MessageComponent} from './message/message.component';
-import {DialogComponent} from './dialog/dialog.component';
 import {CalendarComponent} from './calendar/calendar.component';
 import {helpToCalendar} from './demo/demo.component';
 import {SendComponent} from './send/send.component';
@@ -42,5 +41,5 @@ const routes: Routes = [
 export class AppRoutingModule { }
 //zaimportowałem ją w app.module w 3 klinijce
 export const RoutingComponent = [StudentsComponent, MainComponent, LoginComponent, PlanComponent, ClassComponent,
-  GradesComponent, TeachersComponent, MessageComponent, DialogComponent, CalendarComponent, SendComponent, NoticeComponent,
+  GradesComponent, TeachersComponent, MessageComponent, CalendarComponent, SendComponent, NoticeComponent,
   helpToCalendar, SendNoticeComponent, Plan1aComponent, GradesDetailComponent];
